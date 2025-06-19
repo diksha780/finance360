9d722f77f666f09ab7de3a5438509f5bcf4d6827
Diksha Nangla <112332164+diksha780@users.noreply.github.com>
2025-06-19 09:34:18 +0530

    Update page.jsx

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -16,7 +16,7 @@ export default async function AddTransactionPage({ searchParams }) {
   return (
     <div className="max-w-3xl mx-auto px-5">
       <div className="flex justify-center md:justify-normal mb-8">
-        <h1 className="text-5xl  bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text; ">
+        <h1 className="text-5xl  text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2; ">
           {editId ? "Edit" : "Add"} Transaction
           {/* Add Transaction */}
         </h1>
